3061fdec4d03763f3fb1767f9659485a3e5ee9ef
Scott Allen <71349891+swa34@users.noreply.github.com>
2021-07-23 18:16:07 -0400

    Update tail.c

--- tail.c
+++ tail.c
@@ -1,70 +1,99 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-#define MAXLEN 1000
+#define DEFLINES 10 /* default # of lines to print */
+#define LINES   100 /* maximum # of lines to print */
+#define MAXLEN  100 /* maximum length of an input line */
 
-int tail(char *buffer[], int n);
-int read_line(char *line, int max);
-void print_lines(char* buffer[], int n);
+void error(char *);
+int mgetline(char *,int);
 
-static int lines_read = 0;
+/* print the last n lines of the input */
 
-int main(int argc, char* argv[]) {
-  int number_of_lines = 10;
-  char c;
+int main(int argc,char *argv[])
+{
+    char *p;
+    char *buf;  /* pointer to the large buffer */
+    char *bufend;   /* end of the large buffer */
 
-  while(--argc > 0 && (*++argv)[0] == '-') {
-    while((c = *++argv[0])) {
-      switch(c) {
-        case 'n':
-          if(argc-1 > 0)
-            number_of_lines = atoi(*(argv+1));
-          break;
-      }
+    char line[MAXLEN];
+    char *lineptr[LINES];   /* pointer to lines read */
+    
+    int first,i,last,len,n,nlines;
+
+    if( argc == 1)
+        n = DEFLINES;
+
+    else if(argc ==2 && (*++argv)[0] == '-')
+        n = atoi(argv[0]+1);
+    else
+        error("Usage: tail [-n]");
+
+    if( n < 1 || n > LINES)
+            n = LINES;
+
+    for(i = 0; i < LINES; i++)
+            lineptr[i] = NULL;
+
+    if(( p = buf = malloc(LINES * MAXLEN)) == NULL)
+        error("tail: cannot allocate buf");
+    bufend = buf + LINES + MAXLEN;
+
+    last = 0;
+    nlines = 0;
+
+    while((len = mgetline(line,MAXLEN)) > 0)
+    {
+        if(p+len+1 >= bufend)
+            p = buf;
+        lineptr[last] = p;
+
+        strcpy(lineptr[last],line);
+        if(++last >= LINES)
+            last = 0;
+
+        p += len + 1;
+        nlines++;
     }
-  }  
 
-  char *line_buffer[number_of_lines];
+    if( n > nlines)
+        n = nlines;
 
-  if(tail(line_buffer, number_of_lines) < 0) {
-    printf("ERROR: failed to allocate memory for a line.\n");
-    return -1;
-  }
+    first = last - n;
 
-  printf("Output:\n"); 
-  print_lines(line_buffer, number_of_lines);
+    if(first < 0)
+        first += LINES;
+    
+    for(i= first; n-- > 0;i = (i+1) % LINES)
+        printf("%s",lineptr[i]);
 
-  return 0;
+    return 0;
 }
 
-int tail(char *buffer[], int n) {
-  int characters_read = 0;
-  char *p, line[MAXLEN];
+/* error: print error messages and exit */
 
-  while((characters_read = read_line(line, MAXLEN)) > 0) {
-    if((p = malloc(sizeof(char)*characters_read)) == NULL) {
-      return -1;
+void error(char *s)
+{
+    printf("%s\n",s);
+    exit(1);
+}
+
+/* mgetline: read a line into s and return length */
+
+int mgetline(char s[],int lim)
+{
+    int c,i;
+    
+    for(i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';++i)
+        s[i] = c;
+    if ( c == '\n')
+    {
+        s[i] = c;
+        ++i;
     }
-    strcpy(p, line);
-    buffer[lines_read++ % n] = p;
-  }
 
-  return 0;
+    s[i] = '\0';
+    return i;
 }
 
-int read_line(char *line, int max) {
-  int c, chars_read = 0;
-
-  while((c = getchar()) != EOF && c != '\n' &&  ++chars_read < max-1)
-    *line++ = c;
-
-  *line = '\0';
-
-  return chars_read;
-}
-
-void print_lines(char *buffer[], int n) {
-  for(int i=0; i < (lines_read > n ? n : lines_read); i++)
-    printf("%s\n", buffer[lines_read > n ? lines_read++ % n : i]);
-}
